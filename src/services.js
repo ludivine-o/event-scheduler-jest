9b75613f44545f4d26529ad8707d592e0bc39a9b
ludivine-o <ludivine.ottini@le-campus-numerique.fr>
2020-08-28 13:31:03 +0200

    ajout des tests et des fonctions

--- src/services.js
+++ src/services.js
@@ -31,7 +31,14 @@ export default class EventService {
      * @return {null | Event}
      */
     getFirstEvent() {
-        return null; //TODO
+        const events = this._eventRepository.getAll()
+        let firstEvent = events[0];
+        for (let i = 1; i < events.length; i++) {
+            if (events[i - 1].getStartTime() > events[i].getStartTime()) {
+                firstEvent = events[i]
+            }
+        }
+        return firstEvent;
     }
 
     /**
@@ -39,7 +46,14 @@ export default class EventService {
      * @return {null | Event}
      */
     getLastEvent() {
-        return null; //TODO
+        const events = this._eventRepository.getAll()
+        let lastEvent = events[0];
+        for (let i = 1; i < events.length; i++) {
+            if (events[i].getStartTime() > events[i - 1].getStartTime()) {
+                lastEvent = events[i]
+            }
+        }
+        return lastEvent;
     }
 
     /**
@@ -47,7 +61,21 @@ export default class EventService {
      * @return {null | Event}
      */
     getLongestEvent() {
-        return null; //TODO
+        const events = this._eventRepository.getAll()
+        let i = 0;
+        let longestEvent
+        let longestEventLength = 0;
+        do {
+            events[i] = this.sanitiseEvent(events[i])
+            if ((events[i].getEndTime() - events[i].getStartTime()) > longestEventLength) {
+                longestEvent = events[i];
+                longestEventLength = longestEvent.getEndTime() - longestEvent.getStartTime()
+            }
+
+            i++
+        }
+        while (i < events.length)
+        return longestEvent;
     }
 
     /**
@@ -55,7 +83,22 @@ export default class EventService {
      * @return {null | Event}
      */
     getShortestEvent() {
-        return null; //TODO
+        const events = this._eventRepository.getAll()
+        let i = 0;
+        let shortestEvent
+
+        let shortestEventLength = this.getLongestEvent().getEndTime() - this.getLongestEvent().getStartTime();
+        do {
+            events[i] = this.sanitiseEvent(events[i])
+            if ((events[i].getEndTime() - events[i].getStartTime()) < shortestEventLength) {
+                shortestEvent = events[i];
+                shortestEventLength = shortestEvent.getEndTime() - shortestEvent.getStartTime()
+            }
+
+            i++
+        }
+        while (i < events.length)
+        return shortestEvent;
     }
 
     // A implementer en TDD
@@ -78,7 +121,13 @@ export default class EventService {
      * @return {null | Event}
      */
     getEventByTitle(title) {
-        return null
+        let evts = this._eventRepository.getAll();
+        for (let i = 0; i < evts.length; i++) {
+            if (evts[i].title == title) {
+                return evts[i]
+            }
+        }
+        return null;
     }
 
     // A implementer en TDD
@@ -87,7 +136,19 @@ export default class EventService {
      * @param {Date} time
      */
     isLocationAvailable(time) {
+        let evts = this._eventRepository.getAll();
+        if (time instanceof Date){
+            for (let i = 0; i < evts.length; i++) {
+                let ev = this.sanitiseEvent(evts[i])
+                if (time >= ev.getStartTime() && time <= ev.getEndTime()){
+                    return false
+                }
+            }
+            return true
+        }
+        return null
     }
+
 
     /**
      * Get current events
@@ -97,5 +158,14 @@ export default class EventService {
         let now = Date.now();
         return this.hasEventOn(new Date(now));
     }
-    
+
+    sanitiseEvent(event) {
+        if (event.getStartTime() > event.getEndTime()) {
+            let newEvent = new Event(new Date(event.getEndTime()), new Date(event.getStartTime()), event.title, event.location, event.description)
+            return newEvent
+        }
+        return event
+    }
 }
+
+
--- test/services.test.js
+++ test/services.test.js
@@ -4,32 +4,116 @@ import EventService from "../src/services";
 jest.mock("../src/repository");
 
 
-describe("Event Service",()=> {
+describe("Event Service", () => {
 
     beforeEach(() => {
         // Clear all instances and calls to constructor and all methods:
         EventRepository.mockClear();
         EventRepository.mockImplementation(() => {
             return {
-                getAll: () => fakeEvents.slice()
+                getAll: () => {
+                    if (fakeEvents != null) {
+                        return fakeEvents.slice()
+                    } else {
+                        return null;
+                    }
+                }
             }
         });
     });
 
-    let fakeEvents = [
-        new Event(new Date('2019-12-17T03:24:00'),new Date('2019-12-17T13:24:00'),"Hello World","Campus Numerique","This is an hello world.."),
-        new Event(new Date('2018-12-17T03:24:00'),new Date('1995-12-17T03:24:00'),"First event","Campus Numerique","This is an hello world.."),
-        new Event(new Date('2020-04-01T09:00:00'),new Date('2020-04-01T17:00:00'),"Unit test againt","Campus Numerique","This is an hello world..")
+    let fakeEvents;
+    const fullFakeEvents = [
+        new Event(new Date('2019-12-17T03:24:00'), new Date('2019-12-17T13:24:00'), "Hello World", "Campus Numerique", "This is an hello world.."),
+        new Event(new Date('2018-12-17T03:24:00'), new Date('1995-12-17T03:24:00'), "First event", "Campus Numerique", "This is an hello world.."),
+        new Event(new Date('2020-04-01T09:00:00'), new Date('2020-04-01T17:00:00'), "Unit test againt", "Campus Numerique", "This is an hello world..")
     ];
+    const emptyFakeEvent = []
+    const nullFakeEvent = null
 
     test('getEvents shall call repository', async () => {
+        fakeEvents = fullFakeEvents;
         let eventService = new EventService(new EventRepository());
         eventService.getEvents();
         expect(EventRepository).toHaveBeenCalledTimes(1);
     })
 
-    test('getEvents shall return 4 result', async () => {
+    test('getEvents shall return 3 result', async () => {
+        fakeEvents = fullFakeEvents;
         let eventService = new EventService(new EventRepository());
         expect(eventService.getEvents().length).toBe(3);
     })
+
+    test('getEvents sahll return null if repo is empty', async () => {
+        fakeEvents = emptyFakeEvent;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getEvents().length).toBe(0);
+    })
+
+    test('getEvents sahll return null if repo is null', async () => {
+        fakeEvents = nullFakeEvent;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getEvents()).toBe(null);
+    })
+
+    test('getFirstEvent shall return only the first event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getFirstEvent()).toBe(fakeEvents[1]);
+    })
+
+    test('getLastEvent shall return only the last event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getLastEvent()).toBe(fakeEvents[2]);
+    })
+
+    test('getLongestEvent shall return only the longest event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getFirstEvent()).toBe(fakeEvents[1]);
+    })
+
+    test('getShortestEvent shall return only the shortest event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getShortestEvent()).toBe(fakeEvents[2]);
+    })
+
+    test('hasEventOn shall return the events active on specified date', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.hasEventOn(new Date('2019-12-17T05:24:00'))).toStrictEqual([fakeEvents[0]]);
+        expect(eventService.hasEventOn(new Date('2023-12-17T05:24:00'))).toStrictEqual([]);
+        expect(eventService.hasEventOn(42)).toStrictEqual([]);
+    })
+
+    test('getEventByTitle shall return the specified event if exists', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getEventByTitle('First event')).toBe(fakeEvents[1]);
+        expect(eventService.getEventByTitle('gzgrethz')).toBeNull();
+        expect(eventService.getEventByTitle(42)).toBeNull();
+    })
+
+    test('isLocationAvailable shall return the specified event if exists', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.isLocationAvailable(new Date('2019-12-17T05:24:00'))).toBe(false);
+        expect(eventService.isLocationAvailable(new Date('2022-12-17T05:24:00'))).toBe(true);
+        expect(eventService.isLocationAvailable(42)).toBeNull();
+    })
+
+    test('getCurrentEvents shall return the specified event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.getCurrentEvents()).toStrictEqual([]);
+    })
+
+    test('sanitiseEvent shall return a cleaned event', async () => {
+        fakeEvents = fullFakeEvents;
+        let eventService = new EventService(new EventRepository());
+        expect(eventService.sanitiseEvent(fakeEvents[1])).toStrictEqual(new Event(new Date('1995-12-17T03:24:00'), new Date('2018-12-17T03:24:00'), "First event", "Campus Numerique", "This is an hello world.."))
+    })
+
 });
